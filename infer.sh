5308c2108a1db2e8303c46e95bf03d5f9d47e299
Yoshioka <sss.yoshioka@gmail.com>
2024-03-21 07:40:01 +0900

    🎸 feat: elyza supported

--- infer.sh
+++ infer.sh
@@ -2,12 +2,15 @@
 
 # model=gorilla-llm/gorilla-openfunctions-v2
 # model=mistralai/Mistral-7B-Instruct-v0.2
-model=cyberagent/calm2-7b-chat
+# model=cyberagent/calm2-7b-chat
+model=elyza/ELYZA-japanese-Llama-2-13b-instruct
 
 python -m vllm.entrypoints.openai.api_server \
   --port 4000 \
   --model ${model} \
   --tensor-parallel-size 2 \
   --max-model-len 2048 \
-  --gpu-memory-utilization 0.5 \
-  --chat-template ./template.jinja
+  --gpu-memory-utilization 0.9 \
+  --chat-template ./elyza-instruct.jinja
+
+#   --chat-template ./template.jinja
--- req.py
+++ req.py
@@ -2,17 +2,23 @@ import litellm
 
 litellm.set_verbose=True
 
-model="openai/cyberagent/calm2-7b-chat"
+# model = "openai/cyberagent/calm2-7b-chat"
+model = "openai/elyza/ELYZA-japanese-Llama-2-13b-instruct"
+
+system = "あなたは誠実で優秀な日本人のアシスタントです。"
+# content = "こんにちは"
+content = "仕事の熱意を取り戻すためのアイデアを5つ挙げてください。"
 
 response = litellm.completion(
     model=model,
     api_key="EMPTY",
     api_base="http://localhost:4000/v1",
     messages=[
-        {"role": "USER", "content": "こんにちは"},
+        {"role": "system", "content": system},
+        {"role": "user", "content": content},
     ],
     temperature=0,
-    max_tokens=256
+    max_tokens=512
 )
 
 print(response)
